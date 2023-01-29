6bb8a0ddcf4b68895e84f3faac95e75961c82e98
openerzone <openerzone@Bennyui-MacBookAir.local>
2023-01-29 23:54:46 +0900

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,20 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    render( < App / > );
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('renders react link', () => {
+    render( < App / > );
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+
+test('renders react', () => {
+    render( < App / > );
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
 });
